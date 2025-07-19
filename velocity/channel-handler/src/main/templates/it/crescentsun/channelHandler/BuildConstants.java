c131505078a43f5dd25458d3dcd22934c7c5cd3e
Klyser8 <35845031+Klyser8@users.noreply.github.com>
2025-07-19 19:09:41 +0200

    Removed BuildConstants as I think it's unneccessary now

---
+++ velocity/channel-handler/src/main/templates/it/crescentsun/channelHandler/BuildConstants.java
@@ -0,0 +1,7 @@
+package it.crescentsun.channelHandler;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+}

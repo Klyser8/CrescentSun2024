7b31fa93e545e4b533fda5bd557391926b3669ae
Klyser8 <35845031+Klyser8@users.noreply.github.com>
2025-11-24 22:49:00 +0100

    Add persistence for detonation orbs

--- paper/impl/crescentcraft/src/main/java/it/crescentsun/crescentcraft/CrescentCraft.java
+++ paper/impl/crescentcraft/src/main/java/it/crescentsun/crescentcraft/CrescentCraft.java
@@ -3,23 +3,52 @@ package it.crescentsun.crescentcraft;
 import it.crescentsun.api.artifacts.ArtifactProvider;
 import it.crescentsun.api.artifacts.ArtifactRegistryService;
 import it.crescentsun.api.crescentcore.CrescentPlugin;
+import it.crescentsun.api.crescentcore.CrescentCoreAPI;
+import it.crescentsun.api.crescentcore.data.plugin.PluginDataRegistryService;
+import it.crescentsun.api.crescentcore.data.plugin.PluginDataService;
 import it.crescentsun.crescentcraft.artifact.DetonationOrb;
-import org.bukkit.plugin.java.JavaPlugin;
+import it.crescentsun.crescentcraft.artifact.DetonationOrbManager;
+import it.crescentsun.crescentcraft.artifact.data.DetonationOrbData;
+
+import javax.management.ServiceNotFoundException;
 
 public final class CrescentCraft extends CrescentPlugin implements ArtifactProvider {
 
+    private DetonationOrbManager detonationOrbManager;
+
     @Override
     public void onEnable() {
-
+        initServices();
+        detonationOrbManager = new DetonationOrbManager(this, pluginDataService);
+        getServer().getPluginManager().registerEvents(detonationOrbManager, this);
     }
 
     @Override
     public void onDisable() {
-
     }
 
     @Override
     public void onArtifactRegister(ArtifactRegistryService registryService) {
         registryService.registerArtifact(new DetonationOrb(this));
+    }
+
+    @Override
+    public void onPluginDataRegister(PluginDataRegistryService service) {
+        service.registerDataClass(this, DetonationOrbData.class);
+    }
+
+    public DetonationOrbManager getDetonationOrbManager() {
+        return detonationOrbManager;
+    }
+
+    @Override
+    protected void initServices() {
+        try {
+            pluginDataService = getServiceProvider(PluginDataService.class);
+            crescentCoreAPI = getServiceProvider(CrescentCoreAPI.class);
+        } catch (ServiceNotFoundException e) {
+            getLogger().severe("Service initialization failed: " + e.getMessage());
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 }
